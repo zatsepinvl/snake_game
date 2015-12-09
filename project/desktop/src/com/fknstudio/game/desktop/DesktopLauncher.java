abbea5b03cb377f7d82814e008d5b06faf7c8289
Vladimir Zatsepin <zatsepin.vl@gmail.com>
2015-12-09 15:49:32 +0300

    started

--- project/desktop/src/com/fknstudio/game/desktop/DesktopLauncher.java
+++ project/desktop/src/com/fknstudio/game/desktop/DesktopLauncher.java
@@ -1,12 +1,14 @@
-package com.frkstudio.snakegame.desktop;
+package com.fknstudio.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.frkstudio.snakegame.MyGdxGame;
+import com.fknstudio.game.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width=600;
+		config.height=600;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
